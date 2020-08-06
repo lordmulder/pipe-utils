0bf739ecfd705d287ae7b00d4d2a9530a7a98db8
LoRd_MuldeR <mulder2@gmx.de>
2020-08-06 21:43:18 +0200

    Added version info.

---
+++ src/version.h
@@ -0,0 +1,8 @@
+/******************************************************************************/
+/* Pipe-utils, by LoRd_MuldeR <MuldeR2@GMX.de>                                */
+/* This work has been released under the CC0 1.0 Universal license!           */
+/******************************************************************************/
+
+#define PIPEUTILS_VERSION_MAJOR 1
+#define PIPEUTILS_VERSION_MINOR 1
+#define PIPEUTILS_VERSION_PATCH 0
