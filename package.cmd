d85cda920630b6c184569081698cda54bd1817fd
LoRd_MuldeR <mulder2@gmx.de>
2020-08-11 21:49:09 +0200

    Improved packaging script.

--- package.cmd
+++ package.cmd
@@ -1,6 +1,37 @@
 @echo off
 cd /d "%~dp0"
 
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Get current date
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+set "ISO_DATE="
+
+for /F "usebackq tokens=1" %%a in (`start /WAIT /B "" "%~dp0.\etc\utils\win32\core-utils\date.exe" +"%%Y-%%m-%%d"`) do (
+	set "ISO_DATE=%%a"
+)
+
+if "%ISO_DATE%"=="" (
+	echo Failed to determine the current date!
+	pause
+	goto:eof
+)
+
+set "OUTFILE=%~dp0.\out\pipe-utils.%ISO_DATE%.zip"
+
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Clean-up
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+mkdir "%~dp0.\out" 2> NUL
+del /F "%OUTFILE%" 2> NUL
+
+if exist "%OUTFILE%" (
+	echo Failed to delete existing "%OUTFILE%" file!
+	pause
+	goto:eof
+)
+
 rmdir /Q /S "%~dp0.\out\~package" 2> NUL
 
 if exist "%~dp0.\out\~package" (
@@ -9,7 +40,12 @@ if exist "%~dp0.\out\~package" (
 	goto:eof
 )
 
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Build!
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 set MAKE_NONINTERACTIVE=1
+
 call ".\make.cmd"
 
 if not "%ERRORLEVEL%"=="0" (
@@ -17,17 +53,45 @@ if not "%ERRORLEVEL%"=="0" (
 	goto:eof
 )
 
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Copy binaries
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 mkdir "%~dp0.\out\~package"
 mkdir "%~dp0.\out\~package\sse2"
 mkdir "%~dp0.\out\~package\x64"
 
 copy /Y "%~dp0.\*.txt"                        "%~dp0.\out\~package"
-copy /Y "%~dp0.\*.md"                         "%~dp0.\out\~package"
+copy /Y "%~dp0.\*.md"                        "%~dp0.\out\~package"
 copy /Y "%~dp0.\bin\Win32\Release\*.exe"      "%~dp0.\out\~package"
 copy /Y "%~dp0.\bin\Win32\Release_SSE2\*.exe" "%~dp0.\out\~package\sse2"
 copy /Y "%~dp0.\bin\x64\Release\*.exe"        "%~dp0.\out\~package\x64"
 
 attrib +R "%~dp0.\out\~package\*.*" /S
+
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Create ZIP package
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pushd "%~dp0.\out\~package"
+
+if not "%ERRORLEVEL%"=="0" (
+	pause
+	goto:eof
+)
+
+"%~dp0.\etc\utils\win32\info-zip\zip.exe" -r -9 "%OUTFILE%" "*.*"
+
+if not "%ERRORLEVEL%"=="0" (
+	pause
+	goto:eof
+)
+
+popd
+
+rmdir /Q /S "%~dp0.\out\~package" 2> NUL
+
+attrib +R "%OUTFILE%"
 
 echo.
 echo PACKAGE COMPLETED.
